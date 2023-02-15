438b52e8cb4e5b6d4db2fd40f3cfa982fa581229
JasperLorelai <wolf.haci12@gmail.com>
2023-02-15 01:46:14 +0100

    Replace template with initial project files

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Replugged plugin template
-
-See the [plugin development guide](https://guide.replugged.dev/docs/plugins/getting-started) for more information on using this template.
+<div align="center">
+	<br>
+  <h2>AMOLED Dark Mode</h2>
+  <h4>A plugin <a href="https://replugged.dev/" target="_blank">Replugged</a> Discord client mod</h4>
+  <hr>
+	<a href="https://replugged.dev/install?identifier=JasperLorelai/amoled-dark-mode&source=github" target="_blank">
+    <img src="https://img.shields.io/badge/-Install%20in%20Replugged-blue?style=for-the-badge&logo=none" alt="Downloads">
+  </a>
+</div>
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,9 @@
-import { Injector, Logger, webpack } from "replugged";
+const CLASS_NAME = "theme-amoled";
 
-const inject = new Injector();
-const logger = Logger.plugin("PluginTemplate");
-
-export async function start(): Promise<void> {
-  const typingMod = await webpack.waitForModule<{
-    startTyping: (channelId: string) => void;
-  }>(webpack.filters.byProps("startTyping"));
-  const getChannelMod = await webpack.waitForModule<{
-    getChannel: (id: string) => {
-      name: string;
-    };
-  }>(webpack.filters.byProps("getChannel"));
-
-  if (typingMod && getChannelMod) {
-    inject.instead(typingMod, "startTyping", ([channel]) => {
-      const channelObj = getChannelMod.getChannel(channel);
-      logger.log(`Typing prevented! Channel: #${channelObj?.name ?? "unknown"} (${channel}).`);
-    });
-  }
+export function start(): void {
+  document.body.classList.add(CLASS_NAME);
 }
 
 export function stop(): void {
-  inject.uninjectAll();
+  document.body.classList.remove(CLASS_NAME);
 }
